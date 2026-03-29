4b6aeb72ce59917dfa01b966935a5fdd94577729
Nirmal Kumar <nirmalkumar.snk04@gmail.com>
2026-03-29 19:22:28 +0530

    fix deploy issue

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,7 @@ detector = AIDetector()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
